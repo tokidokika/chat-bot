157a6df18c5d239e274fa822c0312dfbc133d619
Kate Sm <yukaicat@gmail.com>
2020-03-23 16:44:01 +0300

    проставление тегов

--- bd.py
+++ bd.py
@@ -4,86 +4,59 @@ from bs4 import BeautifulSoup
 
 def get_html(url):
     try:
-        result = requests.get(url)
+        result = requests.get(url, timeout=5)
         result.raise_for_status()
         return result.text
     except(requests.RequestException, ValueError):
         # print("Сетевая ошибка")
         return False
 
-def main_page():
-    response = get_html('https://letsportpeople.com/')
-   
-    soup = BeautifulSoup(response, 'html.parser')
-    new_list = soup.find_all('div', class_='featured-media')
-    result_list = []
-    for new in new_list:
-        title = new.find('a')['title']              
-        url = new.find('a')['href']
-        result_list.append({
-            'title': title,
-            'url': url
-        })
-    return result_list
-
-
-def get_date():
-    dates = main_page()
-    for date in dates:
-        get_title = date.get('title').split(' ')
-        year = get_title[len(get_title) - 1]
-        month = get_title[len(get_title) - 2]
-        country = get_title[len(get_title) - 3]
-        city = get_title[len(get_title) - 4]
-        print(year, month, country, city)
-    return year, month, country, city 
-
-def find_tag_h2():
-    page = get_html('https://letsportpeople.com/ru/year-2020-races_ru/')
-    soup = BeautifulSoup(page, 'html.parser')
-    return soup.find_all('h2')
-
-
-def find_tag_h5():
-    page = get_html('https://letsportpeople.com/ru/year-2020-races_ru/')
-    soup = BeautifulSoup(page, 'html.parser')
-    return soup.find_all('h5')
-
 
 def calendar_page():
     response = get_html('https://letsportpeople.com/ru/year-2020-races_ru/')
 
+    # bs_ = BeautifulSoup(response, 'html.parser').find_all('div', recursive=False) 
+    soup = BeautifulSoup(response, 'html.parser')
+    calendar = soup.find_all('div') 
     month_tag = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
 
-    soup = BeautifulSoup(response, 'html.parser')
-    # cross = soup.find_all('strong')
+    monthes = []
+    
 
-    # for crossed in cross:
-    #     if crossed.find('s'):
-    #         continue
+    for month in calendar:
+        mon = month.find('a')
+        if mon:
+            mont = mon.get('id')
+            if mont in month_tag:
+                monthes.append(mont)
+    print(monthes)
+                
+    i = 0
+    for t in calendar:
+        d = {}
+        id_ = t.find('a')
+        if id_:
+            idd = id_.get('id')
+            if idd in month_tag:
+                i += 1
+        tag_s = t.find('strong')
+        if tag_s:
+            tag_strong = tag_s.text
+            d['month_day'] = tag_strong
+        href = t.find('a')
+        if href:
+            links = href.get('href')
+            if links:
+                d['links'] = links
+            event_text = href.text
+            if event_text:
+                d['event_name'] = event_text
+                d['month'] = monthes[i - 1]
         
-
-    calendar = soup.find_all('div', attrs={"class":"right70"})
-    find_h2 = find_tag_h2()
-    find_h5 = find_tag_h5()
-
-    # for event in calendar:
-    #     title = event.find('a')
-    #     if title:
-    #         title_text = title.text
-    #         url = event.find('a')['href']
-
-    for month in find_h2:
-        id_month = month.find('a')['id']
-        if id_month in month_tag:
-            real_month = id_month
-
+           
             
-    for date_month in find_h5:
-        id_date = date_month.find('strong').text
+
 
 calendar_page()
 
-# def has_style_but_no_border(tag):
-#     return tag.has_attr('style') and not tag.has_attr('border:none')
 
