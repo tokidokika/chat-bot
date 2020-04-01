8b2386d9bdc7cfdf5662d091bb0f8d2936f0391c
Kate Sm <yukaicat@gmail.com>
2020-04-01 17:31:38 +0300

    создание базы данных

--- bd.py
+++ bd.py
@@ -1,6 +1,8 @@
 import requests
+
 from bs4 import BeautifulSoup
 
+from database import Event, base, session
 
 def get_html(url):
     try:
@@ -17,8 +19,16 @@ def calendar_page():
 
     soup = BeautifulSoup(response, 'html.parser')
     calendar = soup.find_all('div')
+    
     month_tag = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-
+    countries = {'Швейцария': 'Швейцария', 'Норвегия': 'Норвегия', 'Израиль': 'Израиль', 'Марокко': 'Марокко', 'Индия': 'Индия', 'Испания': 'Испания', 'Португалия':'Португалия', 
+                'Оаэ': 'ОАЭ', 'Тайланд': 'Тайланд', 'Гонконг': 'Гонконг', 'Италия': 'Италия', 
+                'Оман': 'Оман', 'Россия': 'Россия', 'Хорватия': 'Хорватия', 'Турция': 'Турция', 'Мальта': 'Мальта', 'Япония': 'Япония', 
+                'Греция': 'Греция', 'Кипр': 'Кипр', 'Германия': 'Германия', 'Венгрия': 'Венгрия', 'Польша': 'Польша', 'Казахстан': 'Казахстан',
+                'Австрия': 'Австрия', 'Грузия':'Грузия', 'Беларусь': 'Беларусь', 'Чехия': 'Чехия', 'Финляндия': 'Финляндия', 'Дания':'Дания', 
+                'Латвия': 'Латвия', 'Швеция': 'Швеция', 'Литва': 'Литва', 'Исландия': 'Исландия', 'Словакия': 'Словакия', 'Эстония': 'Эстония', 
+                'Украина': 'Украина', 'Люксембург': 'Люксембург', 'Молдова': 'Молдова', 'Великобритания': 'Великобритания', 'Сша': 'США', 
+                'Нидерланды': 'Нидерланды', 'Сербия': 'Сербия', 'Юар': 'ЮАР', 'Франция': 'Франция', 'Вьетнам': 'Вьетнам'}
     monthes = []
     
 
@@ -28,46 +38,59 @@ def calendar_page():
             mont = mon.get('id')
             if mont in month_tag:
                 monthes.append(mont)
-    
-                
+                                   
     i = 0
     for t in calendar:
-        d = {}
+        d = {}   # словарь, куда записываем все нужные данные со страницы и на основании которого строится БД
 
         id_ = t.find('a')
         if id_:
             idd = id_.get('id')
             if idd in month_tag:
                 i += 1
-
+                
         tag_s = t.find('h5')
         if tag_s:
             tag_day = tag_s.text
-            d['month_day'] = tag_day
             decline = t.find('s')
             if decline:
                 d['decline'] = 'Отменено'
+            else:
+                d['decline'] = 'Без изменений'
 
             href = t.find('a')
             if href:
                 links = href.get('href')
                 if links:
                     d['links'] = links
+                
+                country = t.find_all('div',{'class':'right70'})[0].get_text(separator=' ')
+                country1 = country.title().replace(',', '').split(' ')
+
+                for st in country1:
+                    if st in countries:
+                        d['country'] = countries[st]
                     
                 event_text = href.text
                 if event_text:
-                    d['event_name'] = event_text
-                    if month:
-                        d['month'] = monthes[i - 1]
+                    if 'Календарь забегов' in event_text:
+                        continue
+                    else:
+                        d['event_name'] = event_text
+                        d['date'] = tag_day + " " + monthes[i - 1] + " 2020"
 
                     dist = t.find_all('strong')
                     if dist:
                         dist = dist[-1]
-                        distance = dist.text
-                        d['distance'] = distance        
-                    
-                        print(d)                    
-           
-calendar_page()
+                        d['distance'] = dist.text   
+                        d['race_type'] = 'Шоссейный' 
+                        save_event(d)
 
+        
 def save_event(d):
+    event_data = Event(date=d['date'], decline=d['decline'], links=d['links'], event=d['event_name'], distance=d['distance'], places=d['country'], race_type=d['race_type'])
+    session.add(event_data)
+    session.commit()
+
+
+calendar_page()
--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import sqlalchemy
-import os
 
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
@@ -8,31 +7,35 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
 # флаг echo влючает ведение лога через стандартный модуль logging. Когда он включен, мы видим все созданные SQL-запросы
-engine = create_engine('sqlite:///:memory:', echo=True)
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-print(basedir)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'botapp.db')
+engine = create_engine('sqlite:///mydb.db', echo=False)
 
 base = declarative_base()
 
 class Event(base):
     __tablename__ = 'events'
     id = Column(Integer, primary_key=True)
-    month = Column(String, nullable=False)
+    date = Column(String, nullable=True)
     decline = Column(String, nullable=True)
-    links = Column(String, unique=True, nullable=False)
-    event = Column(String, nullable=False)
-    day = Column(Integer, nullable=False)
+    links = Column(String, nullable=True)
+    event = Column(String, nullable=True)
     distance = Column(String, nullable=True)
+    places = Column(String, nullable=True)
+    race_type = Column(String, nullable=True)
+
+    def __init__(self, date, decline, links, event, distance, places, race_type):
+        self.date = date
+        self.decline = decline
+        self.links = links
+        self.event = event
+        self.distance = distance
+        self.places = places
+        self.race_type = race_type
 
     def __repr__(self):
-        return '<Occasion (event="{}", links="{}")>'.format(self.event, self.links)
+        return '<Occasion (event="{}", links="{}", date="{}", distance="{}", decline="{}", places="{}", race_type="{}")>'.format(
+            self.event, self.links, self.date, self.day, self.distance, self.declien, self.places, self.race_type)
         
 base.metadata.create_all(engine)
 
 session = sessionmaker(bind=engine)()
-event_name = Event(event='chto', links = 'nibud')
-session.add(event_name)
-print(session.new)
 
